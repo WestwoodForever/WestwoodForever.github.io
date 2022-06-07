469534b7709c5adaef274b1ee7fb2e26c15a62e5
wwforever <wwforever80@gmail.com>
2022-06-07 22:49:03 +0900

    #1 graphql query 알아보기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Text from 'components/Test';
 
 const IndexPage = () => {
-  return <Text text="Home" />;
+  return <Text text={'Home'} />;
 };
 
 export default IndexPage;
