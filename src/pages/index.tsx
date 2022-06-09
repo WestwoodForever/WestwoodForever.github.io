1df3880131fa58fd374a164b2c234321b5f5b5b9
wwforever <wwforever80@gmail.com>
2022-06-09 22:52:30 +0900

    #1 Emotionjs 실습환경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Text from 'components/Test';
 import { Link } from 'gatsby';
 
 const IndexPage = () => {
   return (
     <div>
-      <Text text={'Hello, World!'} />
       <Link to="/info/">To Info</Link>
     </div>
   );
