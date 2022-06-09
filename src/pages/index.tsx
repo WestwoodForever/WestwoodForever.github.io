1133cdb638c825ac2125149b7ce77e8e84a5388d
wwforever <wwforever80@gmail.com>
2022-06-09 22:47:46 +0900

    #1 Gatsby에서의 페이지 이동 방법

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Text from 'components/Test';
+import { Link } from 'gatsby';
 
 const IndexPage = () => {
-  return <Text text={'Home'} />;
+  return (
+    <div>
+      <Text text={'Hello, World!'} />
+      <Link to="/info/">To Info</Link>
+    </div>
+  );
 };
 
 export default IndexPage;
